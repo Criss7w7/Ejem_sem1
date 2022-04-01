136218643901127307868fd23a59358a6941f07c
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-04-01 16:03:58 -0300

    Add files via upload

---
+++ Miprimeraaplicacion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacion;
+
+/**
+ *
+ * @author zc201
+ */
+public class Miprimeraaplicacion {
+public static void main (String[] args) {
+        String NombreCompleto, Direccion, EstadoCivil;
+        int Edad;
+        double GastoDiarios;
+        char Genero;
+        boolean poseeVehiculo;
+        
+        NombreCompleto = "Camila Espoeranza Rosale";
+        Direccion = " Calle la amargura desvio  la tristeza";
+        EstadoCivil = "Soltera";
+        Edad = 30;
+        GastoDiarios = 34.45;
+        Genero = 'f';
+        poseeVehiculo = false;
+        
+        System.out.println("Nombre completo: "+ NombreCompleto);
+        System.out.println("Direccion: "+ Direccion);
+        System.out.println("Estadpo Civil: "+ EstadoCivil);
+         System.out.println("Edad: "+ Edad);
+          System.out.println("Gastos Diarios: "+ GastoDiarios);
+           System.out.println("Genero: "+ Genero);
+            System.out.println("Pose Vehiculo: "+poseeVehiculo);
+        
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+
+public class Variables {
+     
+    public static void main(String[] args) {
+     int suma, a,b;
+     a = 12;
+     b = 30;
+     suma = a+b;
+     System.out.println ("La suma es: " + suma);
+    }
+    
+}
